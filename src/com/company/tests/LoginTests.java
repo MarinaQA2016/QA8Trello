a85f1c8d6a5193bd28f16f20b953d9dcf1048a8b
MarinaQA2016 <marinaqatest2016@gmail.com>
2021-03-09 17:03:59 +0200

    HW Sel-04, Sel-05

--- src/com/company/tests/LoginTests.java
+++ src/com/company/tests/LoginTests.java
@@ -47,6 +47,59 @@ public class LoginTests {
 
     }
 
+    @Test
+    public void loginPositive() throws InterruptedException {
+        // ---------Press login button  ---
+        driver.findElement(By.xpath("//a[contains(text(),'Log in')]")).click();
+        Thread.sleep(2000);
+
+
+        //---- Fill in login-field and press "login with Attlassian"----
+        WebElement loginField = driver.findElement(By.id("user"));
+        fillField(loginField,"marinaqatest2019@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(2000);
+
+        //----- Fill in password field and press login-submit button-----------
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).sendKeys("marinaqa");
+        Thread.sleep(2000);
+        driver.findElement(By.id("login-submit")).click();
+
+        //------Wait the Home page loading and print 'Boards' button -------
+        Thread.sleep(20000);
+        System.out.println("Name of the button 'Boards': " + driver
+                .findElement(By.xpath("//button[@aria-label = 'Open Boards Menu']")).getText());
+    }
+
+    @Test
+    public void negativePasswordIncorrect() throws InterruptedException {
+        // ---------Press login button  ---
+        driver.findElement(By.xpath("//a[contains(text(),'Log in')]")).click();
+        Thread.sleep(2000);
+
+
+        //---- Fill in login-field and press "login with Attlassian"----
+        WebElement loginField = driver.findElement(By.id("user"));
+        fillField(loginField,"marinaqatest2019@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(2000);
+
+        //----- Fill in password field and press login-submit button-----------
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).sendKeys("incorrect");
+        Thread.sleep(2000);
+        driver.findElement(By.id("login-submit")).click();
+
+        //------Wait the error-message and print it -------
+        Thread.sleep(5000);
+        System.out.println("Error-message: " + driver
+                .findElement(By.id("login-error")).getText());
+    }
+
+
     public void fillField(WebElement element, String value) {
         element.clear();
         element.click();
